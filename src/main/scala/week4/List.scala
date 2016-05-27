005fb4507b7b7ea9bc0d672461a2dbfc4eca0bf0
Ricky Zhang <ricky@gtx>
2016-05-26 21:02:44 -0400

    Add variance sample.

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -6,7 +6,7 @@ import week3.{List, Cons, Nil}
   */
 //demo apply and function object. see list_function_object.sc
 object List {
-  def apply(): List[Int] = new Nil[Int]
+  def apply(): List[Int] = Nil
   def apply(x: Int): List[Int] = new Cons[Int](x, apply)
   def apply(x:Int, y:Int): List[Int] = new Cons[Int](x, apply(y))
 }
