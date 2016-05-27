f5c7020e19935168927ac3d0339963daf24dcff9
Ricky Zhang <ricky.zhang@sas.com>
2016-05-27 10:24:06 -0400

    Refactor List package.

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -1,9 +1,39 @@
 package week4
 
-import week3.{List, Cons, Nil}
 /**
   * Created by rizhan on 5/24/16.
   */
+//Why covariant? See object test
+trait List[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+  //Note:
+  // def prepend(elem: T): List[T] = new Cons[T](elem, this)
+  // show error -- covariant type T is in contra-variant position. ie T as function argument
+  //  Suppose Empty :< IntSet, NonEmpty :< IntSet
+  //  If xs: List[IntSet],xs prepend Empty is fine.
+  //  If ys: List[NonEmpty], ys prepend Empty is type error
+  // Solution:
+  //  covariant type parameter T may appear in lower bounds of method type parameters
+  //  contra-variant type parameter may appear in upper bounds of method
+  def prepend[U >: T](elem: U): List[U] = new Cons[U](elem, this)
+  override def toString = if (isEmpty) " ." else head + ", " + tail
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+//Note: type Nothing is sub type of all types
+//object is static. It can't be parameterized type
+//It extend List[Nothing]. Nothing is sub type of every type
+object Nil extends List[Nothing] {
+  def isEmpty = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
 //demo apply and function object. see list_function_object.sc
 object List {
   def apply(): List[Int] = Nil
@@ -11,3 +41,10 @@ object List {
   def apply(x:Int, y:Int): List[Int] = new Cons[Int](x, apply(y))
 }
 
+object test {
+  //Nothing <: String
+  //To make List[Nothing] <: List[String] ( so that Nil is sub type of List[String]
+  //declare covariant in List[+T]
+  val x: List[String] = Nil
+ // def f(xs: List[NonEmpty], x: Empty) = xs prepend x
+}
